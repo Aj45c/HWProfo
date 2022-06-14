ccc95a2f28e9c337a6a1c58633523fbd9ac1f9d4
Anthony <anthony.jose53@gmail.com>
2022-06-14 14:25:13 -0400

    added routes

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-[
-    {
-      "name": "Investigator Rick",
-      "live": "https://invrick.netlify.app/",
-      "git": "https://github.com/Aj45c/Investigator-Rick",
-      "image": "https://user-images.githubusercontent.com/24855550/167928146-863800c9-6f21-4619-9c1c-91cd0ab83eed.png"
-    },
-    {
-      "name": "My Manga Rankings",
-      "live": "https://my-manga-rankings.herokuapp.com/manga",
-      "git": "https://github.com/Aj45c/My-Manga-Rankings",
-      "image": "https://user-images.githubusercontent.com/24855550/171687093-46951848-94c5-4ab4-bcce-5ae284053378.png"
-    }
-  ]
+const express = require('express');
+const cors = require("cors");
+const projects = require('./projects.json');
+const about = require("./about.json");
+
+const app = express();
+
+//Middleware----------------------------------
+app.use(cors());
+
+//HOME----------------------------------------
+app.get("/", (req, res) => {
+    res.send("Hey there")
+});
+
+//ROUTES--------------------------------------
+app.get("/projects", (req, res) => {
+    res.json(projects)
+});
+
+app.get('/about', (req, res) =>{
+    res.json(about)
+});
