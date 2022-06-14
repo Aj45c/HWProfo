34ad70b90d2ce989b7f9fe4d797e5432d79c2d56
Anthony <anthony.jose53@gmail.com>
2022-06-14 15:05:22 -0400

    x

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const cors = require("cors");
-const mongoose = require('mongoose');
-const morgan = require('morgan');
+// const mongoose = require('mongoose');
+// const morgan = require('morgan');
 const projects = require('./projects.json');
 const about = require("./about.json");
 
 const app = express();
 
 require('dotenv').config();
-
-const {PORT = 4000, MONGODB_URL} = process.env;
-
-mongoose.connect(MONGODB_URL);
-
-// Mongo Status Listeners
-mongoose.connection
-.on('connected', () => console.log('Connected to MongoDB'))
-.on('error', (err) => console.log('Error with MongoDB: ' + err.message))
 
 //Middleware----------------------------------
 app.use(cors());
@@ -34,7 +25,6 @@ app.get("/projects", (req, res) => {
 app.get('/about', (req, res) =>{
     res.json(about)
 });
+const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-    console.log(`We are a go!: ${PORT}`);
-})
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
