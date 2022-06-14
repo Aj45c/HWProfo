99019de7a2be46eafda53c1d32cf95beff65bb3f
Anthony <anthony.jose53@gmail.com>
2022-06-14 14:35:31 -0400

    In the end...everything is set to go

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const cors = require("cors");
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 const projects = require('./projects.json');
 const about = require("./about.json");
 
 const app = express();
+
+require('dotenv').config();
+
+const {PORT = 4000, MONGODB_URL} = process.env;
+
+mongoose.connect(MONGODB_URL);
+
+// Mongo Status Listeners
+mongoose.connection
+.on('connected', () => console.log('Connected to MongoDB'))
+.on('error', (err) => console.log('Error with MongoDB: ' + err.message))
 
 //Middleware----------------------------------
 app.use(cors());
@@ -21,3 +34,7 @@ app.get("/projects", (req, res) => {
 app.get('/about', (req, res) =>{
     res.json(about)
 });
+
+app.listen(PORT, () => {
+    console.log(`We are a go!: ${PORT}`);
+})
