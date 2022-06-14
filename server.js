dd6d19584cbc6920e9d36d7f223103fff4b75849
Anthony <anthony.jose53@gmail.com>
2022-06-14 14:19:47 -0400

    In the begining there was....

---
+++ server.js
@@ -0,0 +1,14 @@
+[
+    {
+      "name": "Investigator Rick",
+      "live": "https://invrick.netlify.app/",
+      "git": "https://github.com/Aj45c/Investigator-Rick",
+      "image": "https://user-images.githubusercontent.com/24855550/167928146-863800c9-6f21-4619-9c1c-91cd0ab83eed.png"
+    },
+    {
+      "name": "My Manga Rankings",
+      "live": "https://my-manga-rankings.herokuapp.com/manga",
+      "git": "https://github.com/Aj45c/My-Manga-Rankings",
+      "image": "https://user-images.githubusercontent.com/24855550/171687093-46951848-94c5-4ab4-bcce-5ae284053378.png"
+    }
+  ]
